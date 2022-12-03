57ac8a5ef6ac48c6d32e315a04db51003c9ab556
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-12-03 10:54:10 +0200

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,6 +7,7 @@ import {
   ForbesList,
   CryptoHistory,
 } from 'components';
+import article from '../../data/article.json';
 
 export const App = () => {
   return (
@@ -15,7 +16,15 @@ export const App = () => {
         <Heading marginBottom="50px" textAlign="center">
           Task 1
         </Heading>
-        <BlogCard />
+        <BlogCard
+          poster={article.poster}
+          name={article.name}
+          postedAt={article.postedAt}
+          tag={article.tag}
+          title={article.title}
+          description={article.description}
+          avatar={article.avatar}
+        />
 
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 2
--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,47 @@
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+import {
+  Card,
+  CardHeader,
+  CardPoster,
+  CardBody,
+  Tag,
+  CardTitle,
+  CardText,
+  CardFooter,
+  UserBox,
+  Avatar,
+  UserInfo,
+  UserName,
+  Date,
+} from './BlogCard.styled';
+
+export const BlogCard = ({
+  poster,
+  name,
+  posterAt,
+  tag,
+  title,
+  description,
+  avatar,
+}) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster src={poster} alt={tag} />
+      </CardHeader>
+      <CardBody>
+        <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{description}</CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={avatar} alt={name} />
+          <UserInfo>
+            <UserName>{name}</UserName>
+            <Date>{posterAt}</Date>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
 };
